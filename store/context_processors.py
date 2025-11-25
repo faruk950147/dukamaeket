b104912b29e77eef9f3045e4e99493c5ae2d0ca7
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-25 17:32:21 +0600

    I have added Rest API

--- store/context_processors.py
+++ store/context_processors.py
@@ -2,8 +2,9 @@ from store.models import Category
 
 def store_context(request):
     # Root categories + prefetch all children for efficiency
-    categories = Category.objects.filter(parent=None).prefetch_related(
+    categories = Category.objects.filter(parent=None, status='active').prefetch_related(
         'children',              # Level 1
         'children__children'     # Level 2
     )
-    return {'categories': categories}
+    cats = Category.objects.filter(status='active')
+    return {'categories': categories, 'cats': cats}
