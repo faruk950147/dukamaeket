bbcce9e18e78628dc74692f5f23caebee1baa969
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-11-30 11:46:22 +0600

    I have added Rest API

--- store/models.py
+++ store/models.py
@@ -136,7 +136,6 @@ class Product(models.Model):
     long_des = models.TextField(default='N/A')
     keyword = models.TextField(default='N/A')
     description = models.TextField(default='N/A')
-    sku = models.CharField(max_length=150, default='N/A')
     tag = models.CharField(max_length=150, default='N/A')
 
     deadline = models.DateTimeField(blank=True, null=True)
