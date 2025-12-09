d2380d5a3377c9ceae414d0ea10bc12dd1d38188
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-09 10:31:56 +0600

    I have some changes store

--- static/assets/js/ajax-form.js
+++ static/assets/js/ajax-form.js
@@ -1,35 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    const buttons = document.querySelectorAll(".color-btn");
-    const priceBox = document.getElementById("product-price");
-
-    buttons.forEach(btn => {
-        btn.addEventListener("click", function () {
-
-            const colorID = this.dataset.colorId;
-
-            // Active button update
-            buttons.forEach(b => b.classList.remove("active"));
-            this.classList.add("active");
-
-            // AJAX Request
-            fetch("", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-CSRFToken": "{{ csrf_token }}"
-                },
-                body: JSON.stringify({
-                    "color_id": colorID,
-                    "product_id": "{{ product.id }}"
-                })
-            })
-            .then(res => res.json())
-            .then(data => {
-                priceBox.innerText = data.new_price;
-            });
-
-        });
-    });
-
+$(document).ready(function(){
+    // console.log("JQUERY WORKING");
 });
