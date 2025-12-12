de1e94ebfe7060545ca9b1c443e75eef98ba221e
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-12 20:49:11 +0600

    I have added php

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -27,50 +27,6 @@
 
 
 ****************************************************/
-// Rating system
-
-const stars = document.querySelectorAll('.star');
-let selectedRating = 0;
-
-stars.forEach((star, index) => {
-
-    // Hover -> Highlight
-    star.addEventListener('mouseover', () => {
-        highlightStars(index);
-    });
-
-    // Mouse Out -> Reset to selected rating
-    star.addEventListener('mouseout', () => {
-        clearHighlight();
-    });
-
-    // Click -> Set Rating
-    star.addEventListener('click', () => {
-        selectedRating = index + 1;
-
-        // Save selected stars visually
-        stars.forEach((s, i) => {
-            s.classList.toggle('selected', i < selectedRating);
-        });
-
-        document.getElementById("show-rating").textContent =
-            "You selected: " + selectedRating + " Star";
-    });
-});
-
-// Highlight on hover
-function highlightStars(index) {
-    stars.forEach((star, i) => {
-        star.classList.toggle('highlighted', i <= index);
-    });
-}
-
-// Remove hover highlight, show only selected stars
-function clearHighlight() {
-    stars.forEach((star, i) => {
-        star.classList.toggle('highlighted', i < selectedRating);
-    });
-}
 
 // Main Js File
 
