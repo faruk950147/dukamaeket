fed8ae9526c38db82247aca81e0521ab20613d49
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2026-01-28 08:25:26 +0600

    I have some changes

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 from store.views import(
     HomeView,
     ProductDetailView,
@@ -12,9 +11,9 @@ from store.views import(
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<slug:slug>/<int:id>/', ProductDetailView.as_view(), name='product-detail'),
-    path('get-variant-by-size/', csrf_exempt(GetVariantBySizeView.as_view()), name='get-variant-by-size'),
-    path('get-variant-by-color/', csrf_exempt(GetVariantByColorView.as_view()), name='get-variant-by-color'),
+    path('get-variant-by-size/', GetVariantBySizeView.as_view(), name='get-variant-by-size'),
+    path('get-variant-by-color/', GetVariantByColorView.as_view(), name='get-variant-by-color'),
     path('product-review/', ProductReviewView.as_view(), name='product-review'),
     path('shop/', ShopView.as_view(), name='shop'),
-    path('get-filter-products/', csrf_exempt(GetFilterProductsView.as_view()), name='get-filter-products'),
+    path('get-filter-products/', GetFilterProductsView.as_view(), name='get-filter-products'),
 ]
